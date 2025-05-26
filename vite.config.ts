5b30f4ef485650a0207eae38cbef2ae3c50f1c93
KARTHIK <144221056+KaRthiK15789@users.noreply.github.com>
2025-05-26 23:36:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Forward /webhook to http://localhost:3001/webhook
+      "/webhook": "http://localhost:3001",
+
+      // Forward /send to http://localhost:3001/send
+      "/send": "http://localhost:3001",
+    },
+  },
 });
